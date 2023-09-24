cf6791ece67a298743339a2cb4b9019818678e5f
shima8823 <taiyaki1275@gmail.com>
2023-09-24 22:18:13 +0900

    refactor: fn変数を削除して直接handler関数を返す

--- handler/middleware/recovery.go
+++ handler/middleware/recovery.go
@@ -3,13 +3,12 @@ package middleware
 import "net/http"
 
 func Recovery(h http.Handler) http.Handler {
-	fn := func(w http.ResponseWriter, r *http.Request) {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 		defer func() {
 			if err := recover(); err != nil {
 				http.Error(w, "Internal Server Error", http.StatusInternalServerError)
 			}
 		}()
 		h.ServeHTTP(w, r)
-	}
-	return http.HandlerFunc(fn)
+	})
 }
