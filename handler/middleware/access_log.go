f39efa82846df17393caf51397e541b70e12f2d2
shima8823 <taiyaki1275@gmail.com>
2023-09-25 17:04:35 +0900

    initial commit

---
+++ handler/middleware/access_log.go
@@ -0,0 +1,39 @@
+package middleware
+
+import (
+	"encoding/json"
+	"fmt"
+	"net/http"
+	"time"
+)
+
+type accessLogInfo struct {
+	Timestamp time.Time
+	Latency   int64
+	Path      string
+	OS        string
+}
+
+func AccessLogMiddleware(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		startTime := time.Now()
+		next.ServeHTTP(w, r)
+
+		osname, ok := r.Context().Value(OSNameKey).(string)
+		if !ok {
+			osname = "unknown"
+		}
+		accessLog := accessLogInfo{
+			Timestamp: startTime,
+			Latency:   time.Now().UnixNano() - startTime.UnixNano(),
+			Path:      r.URL.Path,
+			OS:        osname,
+		}
+		accessLogJSON, err := json.Marshal(accessLog)
+		if err != nil {
+			fmt.Println(err)
+			return
+		}
+		fmt.Println(string(accessLogJSON))
+	})
+}
