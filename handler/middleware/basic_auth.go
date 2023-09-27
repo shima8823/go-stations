2dce14298f67131a6175793a62c13bd7908fae1a
shima8823 <taiyaki1275@gmail.com>
2023-09-27 15:37:57 +0900

    initial commit

---
+++ handler/middleware/basic_auth.go
@@ -0,0 +1,21 @@
+package middleware
+
+import (
+	"net/http"
+	"os"
+)
+
+func BasicAuth(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		expectedUserID := os.Getenv("BASIC_AUTH_USER_ID")
+		expectedPassword := os.Getenv("BASIC_AUTH_PASSWORD")
+
+		username, password, ok := r.BasicAuth()
+		if !ok || username != expectedUserID || password != expectedPassword {
+			http.Error(w, "Unauthorized", http.StatusUnauthorized)
+			return
+		}
+
+		next.ServeHTTP(w, r)
+	})
+}
