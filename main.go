8613ba94ab962572fc9b6a6c9925cd97d5bbb9ea
shima8823 <taiyaki1275@gmail.com>
2023-09-28 17:22:29 +0900

    feat: Graceful Shutdownを実装する

--- main.go
+++ main.go
@@ -1,13 +1,19 @@
 package main
 
 import (
+	"context"
 	"log"
-	"os"
-	"time"
 	"net/http"
+	"os"
+	"os/signal"
+	"sync"
+	"time"
+
 	"github.com/TechBowl-japan/go-stations/db"
 	"github.com/TechBowl-japan/go-stations/handler/router"
 )
+
+var wg sync.WaitGroup
 
 func main() {
 	err := realMain()
@@ -50,10 +56,32 @@ func realMain() error {
 	// NOTE: 新しいエンドポイントの登録はrouter.NewRouterの内部で行うようにする
 	mux := router.NewRouter(todoDB)
 
-	// TODO: サーバーをlistenする
-	if err := http.ListenAndServe(port, mux); err != nil {
-		return err
+	server := &http.Server{
+		Addr:    port,
+		Handler: mux,
 	}
 
+	ctx, stop := signal.NotifyContext(context.Background(), os.Interrupt, os.Kill)
+	defer stop()
+
+	wg.Add(1)
+	go func() {
+		defer wg.Done()
+		if err := server.ListenAndServe(); err != http.ErrServerClosed {
+			log.Fatalf("ListenAndServe() error: %v", err)
+		}
+	}()
+
+	<-ctx.Done()
+
+	log.Println("Graceful Shutdown start")
+	shutdownCtx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
+	defer cancel()
+
+	if err := server.Shutdown(shutdownCtx); err != nil {
+		log.Fatalf("Server forced to shutdown: %v", err)
+	}
+	wg.Wait()
+	log.Println("Graceful Shutdown Success")
 	return nil
 }
