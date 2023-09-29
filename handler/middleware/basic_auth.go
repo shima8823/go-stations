ed8edd9e0bb45c094e04cb02118a070d4d13267f
shima8823 <taiyaki1275@gmail.com>
2023-09-29 20:06:55 +0900

    fix: 環境変数が設定されていない場合エラーとする

--- handler/middleware/basic_auth.go
+++ handler/middleware/basic_auth.go
@@ -9,6 +9,10 @@ func BasicAuth(next http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 		expectedUserID := os.Getenv("BASIC_AUTH_USER_ID")
 		expectedPassword := os.Getenv("BASIC_AUTH_PASSWORD")
+		if expectedUserID == "" || expectedPassword == "" {
+			http.Error(w, "server error", http.StatusInternalServerError)
+			return
+		}
 
 		username, password, ok := r.BasicAuth()
 		if !ok || username != expectedUserID || password != expectedPassword {
